5a1eeac5efad581e680792ea09ab8c723ddcb2d1
PhotonMike <tothmiklostibor@gmail.com>
2017-05-18 13:15:56 +0200

    Initial commit

---
+++ src/kotlintry/Controller.java
@@ -0,0 +1,4 @@
+package kotlintry;
+
+public class Controller {
+}
---
+++ src/kotlintry/KotlinMain.kt
@@ -0,0 +1,13 @@
+package kotlintry
+
+import javafx.scene.layout.GridPane
+
+/**
+ * Created by 2013.A on 2017. 05. 18..
+ */
+class KotlinMain {
+    fun start () {
+        val grid = GridPane();
+
+    }
+}
