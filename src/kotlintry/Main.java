3d4633ce829d76e594c91737e374b7bb17bb4643
PhotonMike <tothmiklostibor@gmail.com>
2017-05-19 22:43:05 +0200

    Kotlin!

--- src/kotlintry/Main.java
+++ src/kotlintry/Main.java
@@ -10,11 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        KotlinMain kotlin = new KotlinMain();
         GridPane root =new GridPane();
         primaryStage.setTitle("Kotlin?");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
         Button button = new Button();
+        button.setOnAction(e -> {
+            kotlin.start();
+        });
         root.getChildren().add(button);
     }
 
